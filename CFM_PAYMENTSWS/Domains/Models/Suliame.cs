ace313b772ac360515f4e1c4edca81e013175e1a
2BDevTeam <github@2business-si.com>
2024-09-05 15:55:00 +0200

    sync

---
+++ CFM_PAYMENTSWS/Domains/Models/Suliame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CFM_PAYMENTSWS.Domains.Models
+{
+    public partial class Suliame
+    {
+        public decimal Userno { get; set; }
+        public string Email { get; set; } = null!;
+        public string Tlmvl { get; set; } = null!;
+    }
+}
