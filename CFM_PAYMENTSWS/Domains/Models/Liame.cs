2b64d6abddcafe1edaadd56faedc771f3e405b56
2BDevTeam <github@2business-si.com>
2024-09-05 09:06:01 +0200

    sync

---
+++ CFM_PAYMENTSWS/Domains/Models/Liame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CFM_PAYMENTSWS.Domains.Models
+{
+    public partial class Liame
+    {
+        public string Liamestamp { get; set; } = null!;
+        public string Para { get; set; } = null!;
+        public string Assunto { get; set; } = null!;
+        public string Corpo { get; set; } = null!;
+        public bool Processado { get; set; }
+        public DateTime Ousrdata { get; set; }
+        public string Keystamp { get; set; } = null!;
+    }
+}
