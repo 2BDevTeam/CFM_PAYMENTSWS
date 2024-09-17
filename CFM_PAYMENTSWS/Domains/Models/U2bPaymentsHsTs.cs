161adec09a5ac9d0885dba8a0ceef9cbdc42040d
2BDevTeam <github@2business-si.com>
2024-09-17 10:34:37 +0200

    sync

---
+++ CFM_PAYMENTSWS/Domains/Models/U2bPaymentsHsTs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CFM_PAYMENTSWS.Domains.Models
+{
+    public partial class U2bPaymentsHsTs
+    {
+        public string TransactionId { get; set; } = null!;
+        public string CreditAccount { get; set; } = null!;
+        public string BeneficiaryName { get; set; } = null!;
+        public string TransactionDescription { get; set; } = null!;
+        public string Currency { get; set; } = null!;
+        public decimal Amount { get; set; }
+        public string BankReference { get; set; } = null!;
+        public string StatusCode { get; set; } = null!;
+        public string StatusDescription { get; set; } = null!;
+        public string BatchId { get; set; } = null!;
+        public DateTime ProcessingDate { get; set; }
+        public string StatusCodeHs { get; set; } = null!;
+        public string StatusDescriptionHs { get; set; } = null!;
+        public string U2bPaymentsHsTsstamp { get; set; } = null!;
+        public string DebitAccount { get; set; } = null!;
+        public DateTime Ousrdata { get; set; }
+        public string Oristamp { get; set; } = null!;
+    }
+}
--- CFM_PAYMENTSWS/Persistence/Contexts/TmpDbContext.cs
+++ CFM_PAYMENTSWS/Persistence/Contexts/TmpDbContext.cs
@@ -17,7 +17,7 @@ namespace CFM_PAYMENTSWS.Persistence.Contexts
         {
         }
 
-        public virtual DbSet<U2bPaymentsHs> U2bPaymentsHs { get; set; } = null!;
+        public virtual DbSet<U2bPaymentsQueueTs> U2bPaymentsQueueTs { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -30,98 +30,145 @@ namespace CFM_PAYMENTSWS.Persistence.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<U2bPaymentsHs>(entity =>
+            modelBuilder.Entity<U2bPaymentsQueueTs>(entity =>
             {
-                entity.HasKey(e => e.U2bPaymentsHsstamp);
+                entity.HasKey(e => e.U2bPaymentsQueueTsstamp)
+                    .HasName("pk_u_2b_paymentsQueue_ts")
+                    .IsClustered(false);
 
-                entity.ToTable("u_2b_payments_hs");
+                entity.ToTable("u_2b_paymentsQueue_ts");
 
-                entity.Property(e => e.U2bPaymentsHsstamp)
-                    .HasMaxLength(30)
+                entity.Property(e => e.U2bPaymentsQueueTsstamp)
+                    .HasMaxLength(25)
                     .IsUnicode(false)
-                    .HasColumnName("u_2b_payments_hsstamp")
-                    .HasDefaultValueSql("(left(newid(),(25)))");
-
-                entity.Property(e => e.Amount).HasColumnType("decimal(18, 2)");
-
-                entity.Property(e => e.BankReference)
-                    .HasMaxLength(255)
-                    .IsUnicode(false)
-                    .HasDefaultValueSql("('')");
+                    .HasColumnName("u_2b_paymentsQueue_tsstamp");
 
                 entity.Property(e => e.BatchId)
-                    .HasMaxLength(100)
+                    .HasMaxLength(25)
                     .IsUnicode(false)
+                    .HasColumnName("batchID")
                     .HasDefaultValueSql("('')");
 
                 entity.Property(e => e.BeneficiaryName)
-                    .HasMaxLength(255)
+                    .HasColumnType("text")
+                    .HasColumnName("beneficiaryName");
+
+                entity.Property(e => e.Canal).HasColumnName("canal");
+
+                entity.Property(e => e.Descricao)
+                    .HasMaxLength(50)
                     .IsUnicode(false)
+                    .HasColumnName("descricao")
                     .HasDefaultValueSql("('')");
 
-                entity.Property(e => e.CreditAccount)
-                    .HasMaxLength(255)
+                entity.Property(e => e.Description)
                     .IsUnicode(false)
+                    .HasColumnName("description")
                     .HasDefaultValueSql("('')");
 
-                entity.Property(e => e.Currency)
-                    .HasMaxLength(5)
+                entity.Property(e => e.Destino)
                     .IsUnicode(false)
+                    .HasColumnName("destino")
                     .HasDefaultValueSql("('')");
 
-                entity.Property(e => e.DebitAccount)
-                    .HasMaxLength(100)
+                entity.Property(e => e.Docno)
+                    .HasMaxLength(50)
                     .IsUnicode(false)
-                    .HasColumnName("debitAccount")
-                    .HasDefaultValueSql("(left(newid(),(25)))");
+                    .HasColumnName("docno");
+
+                entity.Property(e => e.Emailf)
+                    .HasColumnType("text")
+                    .HasColumnName("emailf");
+
+                entity.Property(e => e.Estado)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("estado")
+                    .HasDefaultValueSql("('')");
+
+                entity.Property(e => e.Keystamp)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("keystamp");
+
+                entity.Property(e => e.Lordem)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("lordem");
+
+                entity.Property(e => e.Marcada).HasColumnName("marcada");
+
+                entity.Property(e => e.Moeda)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("moeda")
+                    .HasDefaultValueSql("('')");
+
+                entity.Property(e => e.Origem)
+                    .IsUnicode(false)
+                    .HasColumnName("origem")
+                    .HasDefaultValueSql("('')");
 
                 entity.Property(e => e.Oristamp)
                     .HasMaxLength(30)
                     .IsUnicode(false)
-                    .HasColumnName("oristamp")
-                    .HasDefaultValueSql("('')");
+                    .HasColumnName("oristamp");
 
                 entity.Property(e => e.Ousrdata)
-                    .HasColumnType("datetime")
+                    .HasColumnType("date")
                     .HasColumnName("ousrdata")
                     .HasDefaultValueSql("(getdate())");
 
+                entity.Property(e => e.Ousrhora)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("ousrhora")
+                    .HasDefaultValueSql("(left(CONVERT([time],getdate()),(8)))");
+
+                entity.Property(e => e.Ousrinis)
+                    .HasMaxLength(5)
+                    .IsUnicode(false)
+                    .HasColumnName("ousrinis")
+                    .HasDefaultValueSql("('')");
+
                 entity.Property(e => e.ProcessingDate)
                     .HasColumnType("datetime")
-                    .HasColumnName("processingDate")
-                    .HasDefaultValueSql("('')");
+                    .HasColumnName("processingDate");
 
-                entity.Property(e => e.StatusCode)
-                    .HasMaxLength(255)
+                entity.Property(e => e.Tabela)
                     .IsUnicode(false)
-                    .HasDefaultValueSql("('')");
-
-                entity.Property(e => e.StatusCodeHs)
-                    .HasMaxLength(50)
-                    .IsUnicode(false)
-                    .HasColumnName("statusCodeHs")
-                    .HasDefaultValueSql("('')");
-
-                entity.Property(e => e.StatusDescription)
-                    .HasMaxLength(255)
-                    .IsUnicode(false)
-                    .HasDefaultValueSql("('')");
-
-                entity.Property(e => e.StatusDescriptionHs)
-                    .HasMaxLength(100)
-                    .IsUnicode(false)
-                    .HasColumnName("statusDescriptionHs")
+                    .HasColumnName("tabela")
                     .HasDefaultValueSql("('')");
 
                 entity.Property(e => e.TransactionDescription)
-                    .HasMaxLength(255)
+                    .HasMaxLength(100)
                     .IsUnicode(false)
+                    .HasColumnName("transactionDescription")
                     .HasDefaultValueSql("('')");
 
                 entity.Property(e => e.TransactionId)
-                    .HasMaxLength(255)
                     .IsUnicode(false)
+                    .HasColumnName("transactionId");
+
+                entity.Property(e => e.Usrdata)
+                    .HasColumnType("datetime")
+                    .HasColumnName("usrdata")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Usrhora)
+                    .HasMaxLength(8)
+                    .IsUnicode(false)
+                    .HasColumnName("usrhora")
+                    .HasDefaultValueSql("(left(CONVERT([time],getdate()),(8)))");
+
+                entity.Property(e => e.Usrinis)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("usrinis")
                     .HasDefaultValueSql("('')");
+
+                entity.Property(e => e.Valor)
+                    .HasColumnType("decimal(16, 2)")
+                    .HasColumnName("valor");
             });
 
             OnModelCreatingPartial(modelBuilder);
