fec048bb4e02fbb4b153537464883171b562cd42
2BDevTeam <github@2business-si.com>
2024-09-24 12:37:20 +0200

    sync

--- CFM_PAYMENTSWS/Services/PaymentService.cs
+++ CFM_PAYMENTSWS/Services/PaymentService.cs
@@ -68,7 +68,7 @@ namespace CFM_PAYMENTSWS.Services
                     switch (pagamento.canal)
                     {
                         case 105:
-                            //NedBankProcessing(pagamento);
+                            NedBankProcessing(pagamento);
 
                             break;
                         default:
@@ -449,65 +449,7 @@ namespace CFM_PAYMENTSWS.Services
         }
 
 
-        /*
-        public async Task<ResponseDTO> GetGLTransactions(string inicialDate, string finDate, int? pageIndex, int? pageSize)
-        {
-            try
-            {
-                var responseID = logHelper.generateResponseID();
-
-                var e1 = _phcRepository.GetE1ByEstab(1);
-                var para1 = _phcRepository.GetPara1ByDescricao("mc_dataf");
-
-                DateTime mcDataf = DateTime.ParseExact(para1.Valor.Trim(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
-
-
-                DateTime iDate = DateTime.ParseExact(inicialDate, "yyyyMMdd", CultureInfo.InvariantCulture);
-                DateTime finalDate = DateTime.ParseExact(finDate, "yyyyMMdd", CultureInfo.InvariantCulture);
-
-                var glt = _phcRepository.GetGLTransactions(iDate, finalDate, e1.Ncont, mcDataf, pageIndex, pageSize);
-
-                if (glt.Count == 0)
-                {
-                    throw new Exception("No data.");
-                }
-
-
-                var records = glt.Count;
-
-                int pageI = 1;
-                var pageS = glt.Count;
-                int recordsPPage = glt.Count;
-
-                if (pageIndex.HasValue && pageSize.HasValue)
-                {
-                    records = _phcRepository.GetGLTransactionsCount(iDate, finalDate);
-                    pageI = pageIndex.Value;
-                    pageS = pageSize.Value;
-                }
-
-                return new ResponseDTO(new HeaderDTO(records, pageI, pageS, recordsPPage)
-                        , new ResponseCodesDTO("0000", "Success.", responseID), glt, null);
-            }
-
-            catch (FormatException)
-            {
-                return new ResponseDTO(new HeaderDTO(0, 0, 0, 0)
-                        , new ResponseCodesDTO("0404", "Formatação da data inválida. Por favor, utilize 'yyyyMMdd'.", 0), null, null);
-            }
-            catch (Exception ex)
-            {
-                var errorDTO = new ErrorDTO { message = ex?.Message, stack = ex?.StackTrace?.ToString(), inner = ex?.InnerException?.ToString() + "  " };
-                Debug.Print($"GetGLTransactions ERROR {errorDTO}");
-                var finalResponse = new ResponseDTO(new ResponseCodesDTO("0007", "Error", logHelper.generateResponseID()), errorDTO.ToString(), null);
-                logHelper.generateResponseLogJB(finalResponse, logHelper.generateResponseID().ToString(), "GetGLTransactions", errorDTO?.ToString());
-
-                return new ResponseDTO(new HeaderDTO(0, 0, 0, 0)
-                        , new ResponseCodesDTO("0404", errorDTO.message, 0), null, null);
-            }
-        }
-        */
-
+        
     }
 
 }
